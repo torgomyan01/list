ecb8109b5103ccefbc6e0c52635a5b4b2ed755ff
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2022-12-03 23:30:02 +0400

    Add menu site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const {block, none} = {
+    block: 'block',
+    none: 'none'
+}
+
 $('.home-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
@@ -6,3 +11,18 @@ $('.home-slider').slick({
     autoplay: true,
     autoplaySpeed: 5000,
 });
+
+
+$('.menu-site-item h4').on('click', function (){
+    const thisElem = $(this);
+    const nextElement = thisElem.next('.menu-item-children');
+    nextElement.toggle( function (){
+        const elementStyle = $(this).css('display');
+        if(elementStyle === block){
+            thisElem.children('.fa-chevron-down').css('transform', 'rotate(-180deg)')
+        } else {
+            thisElem.children('.fa-chevron-down').css('transform', 'rotate(0)')
+        }
+    })
+
+})
