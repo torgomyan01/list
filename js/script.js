193651dd8bee4ff90b15da92fb42b7b78ce6679b
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2022-12-05 11:08:03 +0400

    End menu site

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
-const {block, none} = {
+const { block, none, active } = {
     block: 'block',
-    none: 'none'
+    none: 'none',
+    active: 'active'
 }
 
 $('.home-slider').slick({
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
     centerPadding: '60px',
     centerMode: true,
     autoplay: true,
     autoplaySpeed: 5000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        }
+    ]
 });
 
 
-$('.menu-site-item h4').on('click', function (){
+$('.menu-site-item h5').on('click', function (){
+    // $('.menu-item-children').hide(500).children('.fa-chevron-down').css('transform', 'rotate(0)');
     const thisElem = $(this);
     const nextElement = thisElem.next('.menu-item-children');
     nextElement.toggle( function (){
@@ -24,5 +42,26 @@ $('.menu-site-item h4').on('click', function (){
             thisElem.children('.fa-chevron-down').css('transform', 'rotate(0)')
         }
     })
-
 })
+
+
+// $(window).on('scroll', function (){
+//     console.log($(window).scrollTop())
+// })
+
+const navMobileIcon = $('.mobile-menu-block');
+
+navMobileIcon.on('click', function (){
+    $(this).toggleClass(active);
+    checkMobileMenu()
+});
+
+
+function checkMobileMenu(){
+    const menuSite = $('.menu-site');
+    if(navMobileIcon.hasClass(active)){
+        menuSite.css('transform', 'translateX(0)')
+    } else {
+        menuSite.css('transform', 'translateX(-100%')
+    }
+}
